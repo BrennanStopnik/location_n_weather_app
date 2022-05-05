26d6d11568f3e2b0a6186761acf40301a7090d6e
Brennan Stopnik <84404689+BrennanStopnik@users.noreply.github.com>
2022-05-05 12:36:49 -0400

    More updates to models, admin and some html

--- mysite/weather/admin.py
+++ mysite/weather/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-# from .models import Location
+from .models import Location
 
-# admin.site.register(Location)
-# admin.site.register(Answer)
+admin.site.register(Location)
+# admin.site.register(Weather)
+
--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-# class Location(models.Model):
-#     location_query = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-#     def __str__(self):
-#         return self.question_text
+class Location(models.Model):
+    location_query = models.CharField(max_length=200)
 
 
-# class Answer(models.Model):
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     # votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.question_text
 
-#     def __str__(self):
-#         return self.choice_text
+
+class Answer(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.choice_text
